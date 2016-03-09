7c25ca6d1c453d9736e31ce79818a538a0c2c9a5
Waqas <waqas.latif@zigron.com>
2016-03-09 23:35:17 +0500

    no message

--- src/main/java/com/stella/utils/App.java
+++ src/main/java/com/stella/utils/App.java
@@ -1,26 +1,23 @@
 package com.stella.utils;
 
-
 /**
- * Shamsi
+ * @author Shamsi
  * @author ali
- *
+ * @author Waqas
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	
-    	try {
-    		//TBD take folder path and read all files iteratively
-    		ParserConfig config = ParserConfig.getInstance();
-    		String filePath = config.get(ParserConfig.DATASET_DIR_PATH);
-    		
-    		CcdaSectionExtractor extractor = new CcdaSectionExtractor();
-    		extractor.extract(filePath);
-    		
-	    } catch (Exception e) {
-	    	e.printStackTrace();
-	    }
+public class App {
+    public static void main(String[] args) {
+
+        try {
+            // TBD take folder path and read all files iteratively
+            final ParserConfig config = ParserConfig.getInstance();
+            final String filePath = config.get(ParserConfig.DATASET_DIR_PATH);
+
+            final CcdaSectionExtractor extractor = new CcdaSectionExtractor();
+            extractor.extract(filePath);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
